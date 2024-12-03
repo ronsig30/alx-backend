8827d1dee07e23db3f03c25e8ee29ea756719b10
ronsig30 <ronaldrogony30@gmail.com>
2024-12-03 13:15:05 +0300

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,28 @@
+import redis from 'redis';
+
+// Create Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for connection errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to publish messages
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
+
+// Publishing messages with delays
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
